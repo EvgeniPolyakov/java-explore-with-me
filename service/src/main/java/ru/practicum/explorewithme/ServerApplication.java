c3d95d55f9cb7091316d7db9c2616213362ef587
Evgeni.Polyakov <jevgenijs.polakovs@gmail.com>
2022-12-02 00:31:45 +0200

    feat: get RestTemplate through RestTemplateBuilder

--- service/src/main/java/ru/practicum/explorewithme/ServerApplication.java
+++ service/src/main/java/ru/practicum/explorewithme/ServerApplication.java
@@ -2,6 +2,7 @@ package ru.practicum.explorewithme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +14,7 @@ public class ServerApplication {
     }
 
     @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+    public RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 }
